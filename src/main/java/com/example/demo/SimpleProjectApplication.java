029c530d22a75c6770efd1ac145b828317007fbf
wzy <992388202@qq.com>
2022-04-18 19:00:39 +0800

    初始化

---
+++ src/main/java/com/example/demo/SimpleProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author DELL_I5
+ */
+@SpringBootApplication
+public class SimpleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleProjectApplication.class, args);
+    }
+
+}
